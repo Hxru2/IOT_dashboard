b51b05d6c6004a28592dd4e841903c78ebcfe359
Hxruyuk1 <nack91251@gmail.com>
2024-08-15 15:34:43 +0700

    ...

--- src/app/api/savedata/route.js
+++ src/app/api/savedata/route.js
@@ -3,32 +3,28 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+client.connect();
+
+
 export async function POST(request) {
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-  });
-
   try {
-    await client.connect();
-    const { LDR, VR, TEMP, DISTANCE } = await request.json();
-
-    const res = await client.query(
-      'INSERT INTO "NRD012" (LDR, VR, TEMP, DISTANCE) VALUES ($1, $2, $3, $4) RETURNING *',
-      [LDR, VR, TEMP, DISTANCE]
-    );
-
+    const { analog1, analog2, digital1, digital2 } = await request.json();
+    // Hash password
+    const res = await client.query('INSERT INTO "ATH034" (analog1, analog2, digital1, digital2) VALUES ($1, $2, $3, $4) RETURNING *', [analog1, analog2, digital1, digital2]);
     return new Response(JSON.stringify(res.rows[0]), {
       status: 201,
       headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
     });
   } catch (error) {
-    console.error('Error:', error);
+    console.error(error);
     return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
       status: 500,
       headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
     });
-  } finally {
-    await client.end();
   }
 }
 
