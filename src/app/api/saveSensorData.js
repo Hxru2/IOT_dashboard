dd2b2cdf8f9beebd5b30439fdfe477b3273467ab
Hxruyuk1 <nack91251@gmail.com>
2024-08-15 11:22:36 +0700

    ...

--- src/app/api/saveSensorData.js
+++ src/app/api/saveSensorData.js
@@ -1,28 +1,29 @@
 import dbConnect from './dbConnect';
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { LDR, VR, TEMP, DISTANCE } = req.body;
+export async function POST(request) {
+  try {
+    // รับข้อมูลจากคำขอ
+    const { LDR, VR, TEMP, DISTANCE } = await request.json();
 
-    try {
-      const query = 'INSERT INTO NRD012 (LDR, VR, TEMP, DISTANCE) VALUES ($1, $2, $3, $4) RETURNING *';
-      const values = [LDR, VR, TEMP, DISTANCE];
-      const result = await dbConnect(query, values);
+    // คำสั่ง SQL สำหรับการเพิ่มข้อมูลลงในฐานข้อมูล
+    const query = 'INSERT INTO NRD012 (LDR, VR, TEMP, DISTANCE) VALUES ($1, $2, $3, $4) RETURNING *';
+    const values = [LDR, VR, TEMP, DISTANCE];
+    
+    // เชื่อมต่อกับฐานข้อมูลและรันคำสั่ง
+    const result = await dbConnect(query, values);
 
-      res.status(201).json({ message: 'Data saved successfully', data: result.rows[0] });
-      return new Response(JSON.stringify({ error: "Internal Server Error" }), {
-        status: 201,
-        headers: { 'Access-Control-Allow-Origin': '*', "Content-Type": "application/json" },
-      });
-    } catch (error) {
-      console.error('Error saving data:', error);
-      res.status(500).json({ message: 'Internal server error' });
-      return new Response(JSON.stringify({ error: error }), {
-        status: 500,
-        headers: { 'Access-Control-Allow-Origin': '*', "Content-Type": "application/json" },
-      });
-    }
-  } else {
-    res.status(405).json({ message: 'Only POST requests are allowed' });
+    // ส่งข้อมูลกลับไปยังผู้ใช้
+    return new Response(JSON.stringify({ message: 'Data saved successfully', data: result.rows[0] }), {
+      status: 201,
+      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error('Error saving data:', error);
+
+    // ส่งข้อผิดพลาดกลับไปยังผู้ใช้
+    return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+      status: 500,
+      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+    });
   }
 }
